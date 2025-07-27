d55a2e330cedff49c7fdbc49a90ec8b6857d69cd
dcmedeiros18 <dc.medeiros@live.com>
2025-07-27 14:48:05 +0100

    fix: enable Cors for Netlify frontend

--- src/main.ts
+++ src/main.ts
@@ -11,17 +11,16 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:8100','http://localhost:8101','http://localhost:8102', 'https://wod-clock.netlify.app'],
-    credentials: true, // se usa cookies ou auth headers
+    origin: ['http://localhost:8100','http://localhost:8101','http://localhost:8102', 'https://wod-clock.netlify.app',],
+    credentials: true, 
   });
-
-  await app.listen(process.env.PORT || 3000);
-
+ 
   // Serve static files
   app.useStaticAssets(join(__dirname, '..', 'public'), {
     prefix: '/',
   });
 
+  // Global validation pipe
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
